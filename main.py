67b184302499acfa0d9236ccc8d205e803ad5bd5
Luke Reed <luke.reed@outlook.com.au>
2023-12-13 16:06:41 +1100

    Find games function - Returns all games for a specific league on a specific date

--- main.py
+++ main.py
@@ -2,29 +2,26 @@ import requests
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-date = "2023-12-15"
 
-url = f"https://fbref.com/en/matches/{date}"
-response = requests.get(url)
+def find_games(league_id, date):
 
-soup = BeautifulSoup(response.content, "html.parser")
+    url = f"https://fbref.com/en/matches/{date}"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
 
-league_id = 9
+    league_games = soup.find("div", id=f"all_sched_2023-2024_{league_id}").find_all("tr")
 
-games_rows = soup.find("div", id=f"all_sched_2023-2024_{league_id}")\
-            .find_all("tr")
+    for game in league_games:
+        h = game.find("td", {"data-stat": "home_team"})
+        a = game.find("td", {"data-stat": "away_team"})
 
-for game in games_rows:
-    h = game.find("td", {"data-stat": "home_team"})
-    a = game.find("td", {"data-stat": "away_team"})
+        if h and a:
+            home_team = h.find("a").get_text()
+            away_team = a.find("a").get_text()
 
-    if h and a:
-        home_team = h.find("a").get_text()
-        away_team = a.find("a").get_text()
-
-
-        print(home_team)
-        print(away_team)
+            print(home_team)
+            print(away_team)
 
 
+find_games(70, "2023-12-15")
 
